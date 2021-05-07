9944e1a0587ccb00a4110aa8c282d0c880795cce
vo55my <hilmydhaifullah@gmail.com>
2021-05-07 23:01:31 +0700

    Menyelesaikan Video Pertemuan 12

---
+++ phpdasar/pertemuan12/detail.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Dhaifullah Hilmy
+203040119
+https://github.com/vo55my/pw2021_203040119.git
+Pertemuan 12 - 7 Mei 2021
+Mempelajari Login dan Registrasi
+*/
+?>
+
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// ambil id dari url
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $mhs['gambar']; ?>"></li>
+    <li>NRP : <?= $mhs['nrp']; ?></li>
+    <li>Nama : <?= $mhs['nama']; ?></li>
+    <li>Email : <?= $mhs['email']; ?></li>
+    <li>Jurusan : <?= $mhs['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $mhs['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $mhs['id']; ?>" onclick="return confirm ('apakah anda yakin?');">Hapus</a></li>
+    <li><a href="index.php">Kembali ke Daftar Mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
---
+++ phpdasar/pertemuan12/hapus.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Dhaifullah Hilmy
+203040119
+https://github.com/vo55my/pw2021_203040119.git
+Pertemuan 12 - 7 Mei 2021
+Mempelajari Login dan Registrasi
+*/
+?>
+
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// mengambil id dari url
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+  echo "<script>
+          alert('data berhasil dihapus');
+          document.location.href = 'index.php';
+        </script>";
+} else {
+  echo "<script>
+          alert('data gagal dihapus');
+          document.location.href = 'index.php';
+        </script>";
+}
+?>
+<!-- selesai -->
